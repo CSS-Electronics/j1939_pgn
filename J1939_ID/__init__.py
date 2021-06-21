8b64cac8e24f909a6647609e15397d8b7f240795
Christian Steiniche <css@csselectronics.com>
2021-06-21 13:07:29 +0200

    Versioneer added

--- J1939_ID/__init__.py
+++ J1939_ID/__init__.py
@@ -1,1 +1,4 @@
 from .J1939_ID import J1939_ID, J1939_PDU
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
+import versioneer
 
 setup(
     name='J1939_ID',
-    version='0.0.1',
     author='Christian Steiniche',
     author_email='css@csselectronics.com',
     packages=['J1939_ID'],
@@ -13,4 +13,6 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=[],
+    cmdclass=versioneer.get_cmdclass(),
+    version=versioneer.get_version(),
 )
