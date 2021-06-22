e268e1b2713c9f0d8634dda30e6493235eee5e56
Christian Steiniche <css@csselectronics.com>
2021-06-22 09:15:54 +0200

    Package name updated

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 import versioneer
 
 setup(
-    name='J1939_ID',
+    name='J1939_PGN',
     author='Christian Steiniche',
     author_email='css@csselectronics.com',
-    packages=['J1939_ID'],
+    packages=['J1939_PGN'],
     scripts=[],
     url="https://github.com/CSS-Electronics/j1939/",
     license='LICENSE',
-    description='Python module for working with SAE J1939 CAN-bus message IDs',
+    description='Python module for working with SAE J1939 PGNs',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     install_requires=[],
